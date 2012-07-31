e0ba2caaccd4f9f6d274d0cf917ec0ecef3b5615
U-DA-POKAH\me <me@da-pokah.(none)>
2012-07-30 21:52:15 -0400

    Made CandleStickChart work

--- CandleStickChart/src/CandleStickChart/CandleStickChart.java
+++ CandleStickChart/src/CandleStickChart/CandleStickChart.java
@@ -5,16 +5,10 @@ import Functions.Functions;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.math.BigDecimal;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.DateAxis;
@@ -22,7 +16,12 @@ import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.axis.SegmentedTimeline;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.CandlestickRenderer;
-import org.jfree.data.xy.*;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.time.MovingAverage;
+import org.jfree.data.xy.AbstractXYDataset;
+import org.jfree.data.xy.DefaultOHLCDataset;
+import org.jfree.data.xy.OHLCDataItem;
+import org.jfree.data.xy.XYDataset;
 import org.jfree.ui.ApplicationFrame;
 import postgreswork.PostgresWork;
 
@@ -71,19 +70,26 @@ public class CandleStickChart extends ApplicationFrame {
         
         XYPlot mainPlot = new XYPlot(dataset, domainAxis, rangeAxis, renderer);
 
+        final long ONE_DAY = 2*390 *60 * 1000;
+        XYLineAndShapeRenderer maRenderer = new XYLineAndShapeRenderer(true, false);
+        XYDataset              maSataset  = MovingAverage.createMovingAverage(dataset, "MA", ONE_DAY, 0);
+        mainPlot.setRenderer(1, maRenderer);
+        mainPlot.setDataset (1, maSataset);
+        
         //Do some setting up, see the API Doc
         renderer.setSeriesPaint(0, Color.BLACK);
-        renderer.setDrawVolume(false);
+        renderer.setDrawVolume(true);
         rangeAxis.setAutoRangeIncludesZero(false);
         domainAxis.setTimeline( SegmentedTimeline.newMondayThroughFridayTimeline() );
 
         //Now create the chart and chart panel
-        JFreeChart chart = new JFreeChart(this.ticker, null, mainPlot, false);
+        JFreeChart chart = new JFreeChart(this.ticker, null, mainPlot, true);
         ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new Dimension(600, 300));
+        chartPanel.setPreferredSize(new Dimension(1200, 400));
 
         this.add(chartPanel);
         this.pack();
+        this.setVisible(true);
     }
     
     protected AbstractXYDataset getDataSet()
@@ -104,10 +110,17 @@ public class CandleStickChart extends ApplicationFrame {
     
     //This method uses yahoo finance to get the OHLC data
     protected OHLCDataItem[] getData() {
-        List<OHLCDataItem> dataItems = new ArrayList<>();
+        List<OHLCDataItem> dataItems = new ArrayList();
         
-        String query = "SELECT interval_start, open, close, high, low, volume_for_interval FROM " + Functions.getTableName(this.interval) + " WHERE ticker = '" + this.ticker + "' AND (interval_start >= " + this.start_time + " AND interval_end <= " + this.end_time + ")";
-        System.out.println(query);
+        String query = "SELECT interval_start, open, close, high, low, volume_for_interval FROM " + Functions.getTableName(this.interval) + " WHERE ticker = '" + this.ticker + "'";
+        if(this.start_time > 0)
+        {
+            query = query + " AND interval_start >= " + this.start_time;
+        }
+        if(this.end_time > 0)
+        {
+            query = query + " AND interval_start <= " + this.end_time;
+        }
         ArrayList res = this.pWork.runQuery(query);
         
         Iterator i = res.iterator();
@@ -115,53 +128,17 @@ public class CandleStickChart extends ApplicationFrame {
         {
             ArrayList r = (ArrayList) i.next();
             
-            Iterator item1 = r.iterator();
-            
-            while(item1.hasNext())
-            {
-                Date date = new Date(((Long)((ArrayList)r.get(0)).get(1)));
-                double open = Double.parseDouble(((BigDecimal)((ArrayList)r.get(1)).get(1)).toString());
-                double close = Double.parseDouble(((BigDecimal)((ArrayList)r.get(2)).get(1)).toString());
-                double high = Double.parseDouble(((BigDecimal)((ArrayList)r.get(3)).get(1)).toString());
-                double low = Double.parseDouble(((BigDecimal)((ArrayList)r.get(4)).get(1)).toString());
-                double volume = Double.parseDouble(((Long)((ArrayList)r.get(5)).get(1)).toString());
-                
-                OHLCDataItem item = new OHLCDataItem(date, open, high, low, close, volume);
-                dataItems.add(item);
-                System.out.println("HERE");
-            }
-            
-        }
-/*        try {
-            String strUrl= "http://ichart.finance.yahoo.com/table.csv?s="+stockSymbol+"&a=0&b=1&c=2008&d=3&e=30&f=2008&ignore=.csv";
-            URL url = new URL(strUrl);
-            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-            DateFormat df = new SimpleDateFormat("y-M-d");
+            Date date = new Date(((Long)((ArrayList)r.get(0)).get(1)));
+            double open = Double.parseDouble(((BigDecimal)((ArrayList)r.get(1)).get(1)).toString());
+            double close = Double.parseDouble(((BigDecimal)((ArrayList)r.get(2)).get(1)).toString());
+            double high = Double.parseDouble(((BigDecimal)((ArrayList)r.get(3)).get(1)).toString());
+            double low = Double.parseDouble(((BigDecimal)((ArrayList)r.get(4)).get(1)).toString());
+            double volume = Double.parseDouble(((Long)((ArrayList)r.get(5)).get(1)).toString());
 
-            String inputLine;
-            in.readLine();
-            while ((inputLine = in.readLine()) != null) {
-                StringTokenizer st = new StringTokenizer(inputLine, ",");
-
-                Date date       = df.parse( st.nextToken() );
-                double open     = Double.parseDouble( st.nextToken() );
-                double high     = Double.parseDouble( st.nextToken() );
-                double low      = Double.parseDouble( st.nextToken() );
-                double close    = Double.parseDouble( st.nextToken() );
-                double volume   = Double.parseDouble( st.nextToken() );
-                double adjClose = Double.parseDouble( st.nextToken() );
-
-                OHLCDataItem item = new OHLCDataItem(date, open, high, low, close, volume);
-                dataItems.add(item);
-            }
-            in.close();
+            OHLCDataItem item = new OHLCDataItem(date, open, high, low, close, volume);
+            dataItems.add(item);
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        //Data from Yahoo is from newest to oldest. Reverse so it is oldest to newest
-        Collections.reverse(dataItems);
-*/
+        
         //Convert the list into an array
         OHLCDataItem[] data = dataItems.toArray(new OHLCDataItem[dataItems.size()]);
 
